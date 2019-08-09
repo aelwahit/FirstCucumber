3bd8a7d031916ba7a7fdfe186fa1245cbcbda002
aelwahit <aelwahit@gmail.com>
2019-08-09 10:34:31 -0500

    biseyler oldu gibi

--- src/test/java/com/cybertek/utilities/Deneme2.java
+++ src/test/java/com/cybertek/utilities/Deneme2.java
@@ -9,16 +9,23 @@ public class Deneme2 {
 
         System.out.println("Ben yine kurcaladim burayi yahu conflict olayini pekistirmek icin");
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 
     }
 }
 =======
+=======
+>>>>>>> Stashed changes
         int x,y;
         x=4; y=5;
 
         System.out.println(x+y);
 
     }
+<<<<<<< Updated upstream
+}
+>>>>>>> Stashed changes
+=======
 }
 >>>>>>> Stashed changes
