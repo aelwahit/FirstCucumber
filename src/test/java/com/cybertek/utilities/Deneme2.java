17384a40b59cd3ac4ef76a866ce4ad74a52b9bef
aelwahit <aelwahit@gmail.com>
2019-08-09 10:21:21 -0500

    deneme2 updated

--- src/test/java/com/cybertek/utilities/Deneme2.java
+++ src/test/java/com/cybertek/utilities/Deneme2.java
@@ -2,9 +2,16 @@ package com.cybertek.utilities;
 
 public class Deneme2 {
 
-  
+    public static void main(String[] args) {
+
+
   System.out.println("baba ne yaptin? Online degisiklik yaptimm");
 
   System.out.println("Ben yine kurcaladim burayi yahu conflict olayini pekistirmek icin");
 
+
+
+
+
+]
 }
