321b3809830f422635d693b8a04811420f346860
aelwahit <aelwahit@gmail.com>
2019-08-09 10:02:38 -0500

    deneme2 hata duzeltildi

--- src/test/java/com/cybertek/utilities/Deneme2.java
+++ src/test/java/com/cybertek/utilities/Deneme2.java
@@ -1,11 +1,10 @@
 package com.cybertek.utilities;
 
 public class Deneme2 {
-<<<<<<< HEAD
 
-=======
   
-  System.out.println("baba ne yaptin? Online degisiklik yaptimm")
+  System.out.println("baba ne yaptin? Online degisiklik yaptimm");
+
   
->>>>>>> bfcf7ec60d78b1a97997fb78841e74582b51ec9b
+
 }
