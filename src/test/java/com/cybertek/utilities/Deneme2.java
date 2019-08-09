95f0ce48b34cdd8a99313a33f81733d70b181a66
aelwahit <aelwahit@gmail.com>
2019-08-09 10:35:57 -0500

    hersey okay deneme2 fixed under utilities

--- src/test/java/com/cybertek/utilities/Deneme2.java
+++ src/test/java/com/cybertek/utilities/Deneme2.java
@@ -8,24 +8,13 @@ public class Deneme2 {
         System.out.println("baba ne yaptin? Online degisiklik yaptimm");
 
         System.out.println("Ben yine kurcaladim burayi yahu conflict olayini pekistirmek icin");
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-
-    }
-}
-=======
-=======
->>>>>>> Stashed changes
         int x,y;
         x=4; y=5;
 
         System.out.println(x+y);
 
+
     }
-<<<<<<< Updated upstream
 }
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
+
