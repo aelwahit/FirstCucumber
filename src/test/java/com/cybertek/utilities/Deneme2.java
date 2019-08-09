dde2d808bbd30127c2cd0364750ef0f975532c17
aelwahit <aelwahit@gmail.com>
2019-08-09 10:22:14 -0500

    deneme2 yine uopdate @10:22

--- src/test/java/com/cybertek/utilities/Deneme2.java
+++ src/test/java/com/cybertek/utilities/Deneme2.java
@@ -5,13 +5,10 @@ public class Deneme2 {
     public static void main(String[] args) {
 
 
-  System.out.println("baba ne yaptin? Online degisiklik yaptimm");
+        System.out.println("baba ne yaptin? Online degisiklik yaptimm");
 
-  System.out.println("Ben yine kurcaladim burayi yahu conflict olayini pekistirmek icin");
-
-
+        System.out.println("Ben yine kurcaladim burayi yahu conflict olayini pekistirmek icin");
 
 
-
-]
+    }
 }
